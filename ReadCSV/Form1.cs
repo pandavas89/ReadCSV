b7b946f45799cda9a5207b7e7b87e5c75adc0838
plankton <mindcpu@gmail.com>
2020-07-30 16:23:52 +0900

    second commit

--- ReadCSV/Form1.cs
+++ ReadCSV/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,108 @@ namespace ReadCSV
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        public static String file_path = null;
+
+        private void Find_file_btn(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            openFileDialog1.InitialDirectory = "C:\\";  // 경로 지정
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file_path = openFileDialog1.FileName;   // 풀 경로를 불러와 저장
+
+                textBox1.Text = file_path.Split('\\')[file_path.Split('\\').Length - 1];
+
+            }
+
+        }
+
+
+        public void DB_btn(object sender, EventArgs e)
+        {
+            string[,] keep = new string[0, 0];
+
+            ArrayList d_name = new ArrayList();
+            ArrayList d_pno = new ArrayList();
+            ArrayList d_mf = new ArrayList();
+
+
+            StreamReader sr = new StreamReader(file_path);
+
+            string pline = sr.ReadLine();
+            string[] ldata = pline.Split(',');
+
+            // 첫 줄 예외
+            int nameIdx = 0;
+            int pNoIdx = 0;
+            int sNoIdx = 0;
+            for (int i = 0; i < ldata.Length; i++)
+            {
+                if (ldata[i] == "사용자이름")
+                {
+                    nameIdx = i;
+                }
+                else if (ldata[i] == "핸드폰번호")
+                {
+                    pNoIdx = i;
+                }
+                else if (ldata[i] == "좌석번호")
+                {
+                    sNoIdx = i;
+                }
+            }
+
+            //
+
+            while (!sr.EndOfStream)
+            {
+                //int k = 0;
+                // 한 줄씩 읽어온다.
+                string line = sr.ReadLine();
+                // , 기준으로 분리
+                string[] data = line.Split(',');
+                // 출력   1 사용자이름, 4 전화번호, 7 좌석번호(성별)
+                //Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+
+                //성별 분류
+                string seatAndSex = data[sNoIdx];
+                string sex = "F";
+
+                if (seatAndSex.Substring(0, 2) == "남자")
+                {
+                    sex = "M";
+                }
+
+                // 좌석 번호
+                //string sNo = seatAndSex.Split(' ')[1].Substring(0, seatAndSex.Split(' ')[1].Length - 1);
+
+                //string[] input = new string[] { data[nameIdx], data[pNoIdx].Substring(4, 9).Remove(4, 1), sex }; 
+                // 이름, 전화번호, 성별
+                // 전화번호 5자리, - 지움 처리
+
+                // 입력
+
+                d_name.Add(data[nameIdx]);
+                d_pno.Add(data[pNoIdx].Substring(4, 9).Remove(4, 1));
+                d_mf.Add(sex);
+
+            }
+            MessageBox.Show("while done");
+            //
+            var Sql_Conn = new Sql_Conn();
+            Sql_Conn.AddData(d_name, d_pno, d_mf);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ ReadCSV/Sql_Conn.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadCSV
+{
+    class Sql_Conn
+    {
+
+
+        // SQL_DB User ID Password
+        string strConn = "Data Source=DEV-SA-PC;Initial Catalog=STUDY_ROOM;"
+                            + "User ID=sa;Password=jack3081$$;";
+
+        SqlConnection Conn = new SqlConnection();
+
+        public void Open()
+        {
+
+            if (Conn.State == System.Data.ConnectionState.Closed)
+            {
+                Conn.ConnectionString = strConn;
+
+                try
+                {
+                    //MessageBox.Show("DB Open!");
+                    Conn.Open(); // 
+
+                }
+                catch (InvalidCastException e)
+                {
+                    MessageBox.Show(e.Message, "DB Error!");
+                }
+            }
+        }
+
+        public System.Data.ConnectionState State()
+        {
+            return Conn.State;
+        }
+
+        // 연결 닫음
+        public void Close()
+        {
+            Conn.Close();
+        }
+
+        public SqlDataReader SCom(string QString)
+        {
+            Open();
+            SqlCommand Command = new SqlCommand(QString, Conn);
+            try
+            {
+                SqlDataReader reader = Command.ExecuteReader();
+                return reader;
+            }
+            catch (InvalidCastException e)
+            {
+                MessageBox.Show(e.Message, "DB Error!");
+                return null;
+            }
+        }
+
+        public int ECom(string QString)
+        {
+            int rint = 0;
+            Open();
+            SqlCommand command = new SqlCommand(QString, Conn);
+            try
+            {
+                rint = command.ExecuteNonQuery();
+            }
+            catch (InvalidCastException e)
+            {
+                MessageBox.Show(e.Message, "DB Error !");
+            }
+            return rint;
+        }
+
+
+        public void AddData(ArrayList d_name, ArrayList d_pno, ArrayList d_mf) //string[] f_name, string[] f_pno, string[] f_mf
+        {
+            //
+            string queryString = " INSERT INTO T_USER";
+            queryString += " (NAME ";
+            queryString += " ,PNO ";
+            queryString += " ,MF) ";
+            queryString += " VALUES ";
+
+            for (int i = 0; i < d_name.Count; i++)
+            {
+                queryString += String.Format(" ('" + "{0}" + "','" + "{1}" + "','" + "{2}" + "'),", d_name[i], d_pno[i], d_mf[i]);
+            }
+
+            queryString = queryString.Substring(0, queryString.Length - 1);
+
+            MessageBox.Show(queryString);
+
+            /*queryString += " ('" + PNOtext + "'";
+            queryString += " ,'" + NAMEtext + "'";
+            queryString += " ,'" + FMCB + "')";*/
+
+
+            ECom(queryString);
+
+            MessageBox.Show("사용자 정보가 추가되었습니다.!");
+
+        }
+    }
+}
